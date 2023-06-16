a9456422c1394b50fc3093da34c3d5cfac167b97
Joe Silber <58699331+joesilber@users.noreply.github.com>
2023-06-16 11:52:02 -0700

    implementation untested but complete

--- crossed_roller_bearing_loads.py
+++ crossed_roller_bearing_loads.py
@@ -10,7 +10,8 @@ import numpy as np
 nsigma = '5\u03C3'  # load probabilty envelope
 Fr = 7291  # N, 5-sigma radial load
 Fa = 8235  # N, 5-sigma axial load
-Fm = 588  # N*m, 5-sigma moment load
+M = 588  # N*m, 5-sigma moment load
+diam = 0.083 # m, bearing diameter
 
 # # units and such 
 N_per_lbf = 4.448
@@ -35,8 +36,7 @@ tab = f'{" ":4}'
 #                              'peak': {name: load * Gpeak_design for name, load in bearing_loads_stationary.items()},
 #                             }
 
-# Fm_single = bearing_independent_loads['peak']['moment'] / (single_bearing_diam/2)
-# Fm_duplex = bearing_independent_loads['peak']['moment'] / duplex_bearing_sep
+Fm = M / (diam/2)
 typ_relative_axial_load = Fa / (24 * 5.6**2)
 print(f'Note relative axial load = Fa / (Z * D^2) ~ {typ_relative_axial_load:.1f} N/mm^2 = {typ_relative_axial_load/N_per_lbf*25.4**2:.1f} lbf/in^2'
       f'\nHere using approximations appropriate to this level (i.e. at 750-1000 lbf/in^2):\n')
@@ -58,21 +58,10 @@ print(f'{tab} ... X1 = {bearing_factors["X1"]:.2f}, Y1 = {bearing_factors["Y1"]:
 print(f'{tab} ... max equiv load, {max_equiv_load:5.0f} N = {max_equiv_load/N_per_lbf:4.0f} lbf, occurs when accel vector is weighted like axial : radial ~ {ratio_for_max_equiv_load:.3f} : 1\n')
 print('')
 
-loads_str = f'{nsigma} loads:'
-            f'\n{tab}Fa = {Fa} N (axial)'
-            f'\n{tab}Fr = {Fr} N (radial)'
-            f'\n{tab}Fm = {Fm} N*m (moment)'
-            f'\nyield the following bearing design loads.'
-bearing_loads_str += f' These include all safety factors.'
-bearing_loads_str += f'\nIn calculating equivalent loads, the moment component is estimated such that single bearings (1x) are'
-bearing_loads_str += f'\nassumed to have a diameter ~ {single_bearing_diam*1000:.0f} mm, while duplex configurations (2x bearings) are assumed to have'
-bearing_loads_str += f'\nan axial separation distance of ~ {duplex_bearing_sep*1000:.0f} mm.\n'
-
-for accel_type, loads in bearing_independent_loads.items():
-    bearing_loads_str += f'\n{tab}{accel_type} radial (independent) = {loads["radial"]:.0f} N = {loads["radial"]/N_per_lbf:.0f} lbf'
-    bearing_loads_str += f'\n{tab}{accel_type} thrust (independent) = {loads["thrust"]:.0f} N = {loads["thrust"]/N_per_lbf:.0f} lbf'
-    bearing_loads_str += f'\n{tab}{accel_type} moment (independent) = {loads["moment"]:.1f} N*m = {loads["moment"]/Nm_per_inlb:.1f} in*lb'
-bearing_loads_str += '\n'
-for bearing_type, load in bearing_equiv_loads.items():
-    bearing_loads_str += f'\n{tab}{bearing_type:<18} ({n_bearings[bearing_type]}x) ... peak equivalent load = {load:.0f} N = {load/N_per_lbf:.0f} lbf'
-print(bearing_loads_str)
+loads_str = f'{nsigma} loads:' \
+            f'\n{tab}Fa = {Fa} N (axial)' \
+            f'\n{tab}Fr = {Fr} N (radial)' \
+            f'\n{tab}Fm = {M} N*m (moment), applied across bearing diameter = {diam*1000} mm' \
+            f'\n... yield static equivalent load = {max_equiv_load:.1f} N' \
+            f'\n                                 = {max_equiv_load/N_per_lbf:.1f} lbf'
+print(loads_str)
