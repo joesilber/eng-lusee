dd9828eabbb95fd51122ff27099e4ee9ec1a6c4b
Joe Silber <58699331+joesilber@users.noreply.github.com>
2023-06-16 11:55:17 -0700

    runs now, removed some outdated comments

--- crossed_roller_bearing_loads.py
+++ crossed_roller_bearing_loads.py
@@ -15,26 +15,7 @@ diam = 0.083 # m, bearing diameter
 
 # # units and such 
 N_per_lbf = 4.448
-# Nm_per_ozin = 1 / 141.61193227806
-# Nm_per_inlb = Nm_per_ozin * 16
 tab = f'{" ":4}'
-# earth_1G = 9.81  # m/s^2
-
-# # component sizes
-# z_cg = 0.0003  # m,  payload center of gravity
-# payload_mass = 5.13  # kg
-# single_bearing_diam = 0.083  # pitch circle diameter of single bearing
-# duplex_bearing_sep = 0.040  # assumed axial separation distance between duplexed bearings (where applicable)
-
-# bearing_supported_load = payload_mass * earth_1G
-# bearing_supported_moment = payload_mass * earth_1G * z_cg
-# bearing_loads_stationary = {'thrust': bearing_supported_load,  # nominally upright
-#                             'radial': bearing_supported_load,  # when tipped 90 deg
-#                             'moment': bearing_supported_moment,  # when tipped 90 deg
-#                            }
-# bearing_independent_loads = {'rms': {name: load * Grms_design for name, load in bearing_loads_stationary.items()},
-#                              'peak': {name: load * Gpeak_design for name, load in bearing_loads_stationary.items()},
-#                             }
 
 Fm = M / (diam/2)
 typ_relative_axial_load = Fa / (24 * 5.6**2)
@@ -56,12 +37,12 @@ max_equiv_load = max(possible_loads.values())
 ratio_for_max_equiv_load = list(possible_loads.keys())[list(possible_loads.values()).index(max_equiv_load)]
 print(f'{tab} ... X1 = {bearing_factors["X1"]:.2f}, Y1 = {bearing_factors["Y1"]:.2f}, e = {bearing_factors["e"]:.2f}, X2 = {bearing_factors["X2"]:.2f}, Y2 = {bearing_factors["Y2"]:.2f}')
 print(f'{tab} ... max equiv load, {max_equiv_load:5.0f} N = {max_equiv_load/N_per_lbf:4.0f} lbf, occurs when accel vector is weighted like axial : radial ~ {ratio_for_max_equiv_load:.3f} : 1\n')
-print('')
 
 loads_str = f'{nsigma} loads:' \
             f'\n{tab}Fa = {Fa} N (axial)' \
             f'\n{tab}Fr = {Fr} N (radial)' \
             f'\n{tab}Fm = {M} N*m (moment), applied across bearing diameter = {diam*1000} mm' \
-            f'\n... yield static equivalent load = {max_equiv_load:.1f} N' \
-            f'\n                                 = {max_equiv_load/N_per_lbf:.1f} lbf'
+            f'\n' \
+            f'\n--> static equivalent load = {max_equiv_load:>8.1f} N' \
+            f'\n                           = {max_equiv_load/N_per_lbf:>8.1f} lbf'
 print(loads_str)
