d7369f36b48f4ac52a6c50ac9242eaff58d7071b
Joe Silber <58699331+joesilber@users.noreply.github.com>
2023-06-16 10:37:12 -0700

    more simplifying to crossed roller case only

--- crossed_roller_bearing_loads.py
+++ crossed_roller_bearing_loads.py
@@ -7,6 +7,7 @@ import numpy as np
 # peak loads from random vibration FEA
 # these are all 5-sigma (99.99994% probability envelope)
 # FEA date 2023-06-15
+nsigma = '5\u03C3'  # load probabilty envelope
 Fr = 7291  # N, 5-sigma radial load
 Fa = 8235  # N, 5-sigma axial load
 Fm = 588  # N*m, 5-sigma moment load
@@ -15,7 +16,7 @@ Fm = 588  # N*m, 5-sigma moment load
 N_per_lbf = 4.448
 # Nm_per_ozin = 1 / 141.61193227806
 # Nm_per_inlb = Nm_per_ozin * 16
-# tab = f'{" ":4}'
+tab = f'{" ":4}'
 # earth_1G = 9.81  # m/s^2
 
 # # component sizes
@@ -40,30 +41,28 @@ typ_relative_axial_load = Fa / (24 * 5.6**2)
 print(f'Note relative axial load = Fa / (Z * D^2) ~ {typ_relative_axial_load:.1f} N/mm^2 = {typ_relative_axial_load/N_per_lbf*25.4**2:.1f} lbf/in^2'
       f'\nHere using approximations appropriate to this level (i.e. at 750-1000 lbf/in^2):\n')
 bearing_factors = {'X1': 1.0, 'Y1': 0.44, 'e': 1.5, 'X2': 0.67, 'Y2': 0.67}  # per IKO catalog
-bearing_equiv_loads = {}
-
-    external_preload = 0.
-
-    possible_loads = {}
-    for r in np.linspace(1e-9, 1-1e-9, 10):  # radial fraction of vector acceleration components
-        a = (1 - r**2)**0.5  # axial fraction of vector acceleration components
-        n = n_bearings[key]
-        load_ratio = (a*Fa) / (r*Fr/n + r*Fm)
-        x = factors['X1'] if load_ratio > factors['e'] else factors['X2']
-        y = factors['Y1'] if load_ratio > factors['e'] else factors['Y2']
-        radial_term = r * Fr * x / n
-        axial_term = a * (Fa + external_preload) * y
-        moment_term = r * Fm
-        # note by convention these load terms add as a straight sum, *not* in quadrature
-        possible_loads[load_ratio] = radial_term + axial_term + moment_term
-    max_load = max(possible_loads.values())
-    ratio_for_max_load = list(possible_loads.keys())[list(possible_loads.values()).index(max_load)]
-    bearing_equiv_loads[key] = max_load
-    print(f'{tab}{key:<18} ... X1 = {factors["X1"]:.2f}, Y1 = {factors["Y1"]:.2f}, e = {factors["e"]:.2f}, X2 = {factors["X2"]:.2f}, Y2 = {factors["Y2"]:.2f}')
-    print(f'{tab}{tab}... max equiv load, {max_load:5.0f} N = {max_load/N_per_lbf:4.0f} lbf, occurs when accel vector is weighted like axial : radial ~ {ratio_for_max_load:.3f} : 1\n')
+possible_loads = {}
+for r in np.linspace(1e-9, 1-1e-9, 10):  # radial fraction of vector acceleration components
+    a = (1 - r**2)**0.5  # axial fraction of vector acceleration components
+    load_ratio = (a*Fa) / (r*Fr + r*Fm)
+    x = bearing_factors['X1'] if load_ratio > bearing_factors['e'] else bearing_factors['X2']
+    y = bearing_factors['Y1'] if load_ratio > bearing_factors['e'] else bearing_factors['Y2']
+    radial_term = r * Fr * x
+    axial_term = a * Fa * y
+    moment_term = r * Fm
+    # note by convention these load terms add as a straight sum, *not* in quadrature
+    possible_loads[load_ratio] = radial_term + axial_term + moment_term
+max_equiv_load = max(possible_loads.values())
+ratio_for_max_equiv_load = list(possible_loads.keys())[list(possible_loads.values()).index(max_equiv_load)]
+print(f'{tab} ... X1 = {bearing_factors["X1"]:.2f}, Y1 = {bearing_factors["Y1"]:.2f}, e = {bearing_factors["e"]:.2f}, X2 = {bearing_factors["X2"]:.2f}, Y2 = {bearing_factors["Y2"]:.2f}')
+print(f'{tab} ... max equiv load, {max_equiv_load:5.0f} N = {max_equiv_load/N_per_lbf:4.0f} lbf, occurs when accel vector is weighted like axial : radial ~ {ratio_for_max_equiv_load:.3f} : 1\n')
 print('')
 
-bearing_loads_str = f'Payload mass of {payload_mass:.2f} kg yields the following bearing design loads.'
+loads_str = f'{nsigma} loads:'
+            f'\n{tab}Fa = {Fa} N (axial)'
+            f'\n{tab}Fr = {Fr} N (radial)'
+            f'\n{tab}Fm = {Fm} N*m (moment)'
+            f'\nyield the following bearing design loads.'
 bearing_loads_str += f' These include all safety factors.'
 bearing_loads_str += f'\nIn calculating equivalent loads, the moment component is estimated such that single bearings (1x) are'
 bearing_loads_str += f'\nassumed to have a diameter ~ {single_bearing_diam*1000:.0f} mm, while duplex configurations (2x bearings) are assumed to have'
